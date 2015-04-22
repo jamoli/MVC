c5325d34d83f7e1d67e9457b1c29728729e2c532
jamoli <jamoli@gmail.com>
2015-04-22 19:38:34 +0200

    MVC
    Hello MVC

---
+++ libs/SPDO.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Clase SPDO (es una clase que extiende de PDO). 
+Su única ventaja es que nos permite aplicar el patrón Singleton para mantener una única instancia de PDO
+*/
+class SPDO extends PDO
+{
+    private static $instance = null;
+ 
+    public function __construct()
+    {
+        $config = Config::singleton();
+        parent::__construct('mysql:host=' . $config->get('dbhost') . ';dbname=' . $config->get('dbname'),
+$config->get('dbuser'), $config->get('dbpass'));
+    }
+ 
+    public static function singleton()
+    {
+        if( self::$instance == null )
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+?>
---
+++ models/ItemsModel.php
@@ -0,0 +1,23 @@
+<?php
+/* Clase modelo que cubre las necesidades funcionales del desarrollo mediante una clase. Usamos PDO (esta vez a través de SPDO) para el acceso a datos.
+*/
+class ItemsModel
+{
+    protected $db;
+ 
+    public function __construct()
+    {
+        //Traemos la única instancia de PDO
+        $this->db = SPDO::singleton();
+    }
+ 
+    public function listadoTotal()
+    {
+        //realizamos la consulta de todos los items
+        $consulta = $this->db->prepare('SELECT * FROM variables');
+        $consulta->execute();
+        //devolvemos la colección para que la vista la presente.
+        return $consulta;
+    }
+}
+?>
